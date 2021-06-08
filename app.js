6a5691a895a2f953501ba3760dcc3d148b3ec45b
LorenaCoheneBaez <loreley.cb15@gmail.com>
2021-06-07 22:18:41 -0300

    Corrijo app.js y home.html

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app=express();//ejecuto express
 const port=3030;//declaro constante para el puerto
 const path=require('path');//requiero path para sacar la ruta
 
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));//get para traer el home, sendFile y path.join para que vaya al home.html
-
 app.use(express.static('public'));//indico que en public están los elementos estáticos,ej: imágenes
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));//get para traer el home, sendFile y path.join para que vaya al home.html
 
 app.listen(port,()=>console.log('Servidor corriendo OK en puerto '+port));//levanto el puerto con .listen
